55da18910da06a496a555953bc7d8af5dbdb0834
yuuma03 <93882972+yuuma03@users.noreply.github.com>
2023-06-02 12:43:20 -0300

    fix: we're already initializing the service worker on index.html

--- scripts.js
+++ scripts.js
@@ -331,13 +331,3 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('savedItems', JSON.stringify([]));
   }
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/seu-service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker registrado com sucesso:', registration);
-    })
-    .catch(function(error) {
-      console.log('Falha ao registrar o Service Worker:', error);
-    });
-}
